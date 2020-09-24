cbca276a9406dc211abf4eaaa72dccd15297f903
Bálint Berente <balint.berente@gmail.com>
2020-09-24 23:26:10 +0200

    Update webserver.js
    Path settings

--- webserver.js
+++ webserver.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname,'bc-frontend', 'build')));
+app.use(express.static(path.join(__dirname,'budgee-client', 'build')));
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname,'bc-frontend', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname,'budgee-client', 'build', 'index.html'));
 });
 app.listen(3000);
 app.addListener('error',(err)=>{
