613bc4a4255effe037448f4d523807214c5e24dd
Bálint Berente <balint.berente@gmail.com>
2020-09-27 19:45:41 +0200

    Dark Mode support and layout corrections

--- budgee-client/src/tabs/Add.js
+++ budgee-client/src/tabs/Add.js
@@ -13,15 +13,23 @@ export class Add extends Component {
         this.inputRef.current.value = "";
     }
 
+    addKeyDown = function (e) {
+        if (e.keyCode === 13) this.addRecord();
+    }
+
     render() {
         return (
             <div className="mainContainer">
                 <div className="topContent">
                     <h1>Add record</h1>
                     <p>Tip: Write negative value for income!</p>
-                    <input type="number" placeholder="Type amount here!" className="textField" ref={this.inputRef}></input>
-                    <input type="button" className="button" value="Add!" onClick={this.addRecord}></input>
-                    <input type="button" className="button deleteButton" value="Delete expenses" onClick={this.props.deleteExpenses}></input>
+                    <div className="tabField">
+                        <input type="number" placeholder="Type amount here!" className="textField" ref={this.inputRef} onKeyDown={this.addKeyDown.bind(this)}></input>
+                        <div className="inline-flex center">
+                            <input type="button" className="button" value="Add!" onClick={this.addRecord}></input>
+                            <input type="button" className="button deleteButton" value="Delete expenses" onClick={this.props.deleteExpenses}></input>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
--- budgee-client/src/tabs/Settings.js
+++ budgee-client/src/tabs/Settings.js
@@ -2,40 +2,40 @@ import React, { Component } from 'react';
 
 export class Settings extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.budgetRef = React.createRef();
         this.nameRef = React.createRef();
         this.editBudget = this.editBudget.bind(this);
         this.editName = this.editName.bind(this);
     }
-    
-    editBudget = function(){
+
+    editBudget = function () {
         var inputIsCorrect = true;
-        var str = this.budgetRef.current.value;
+        //var str = this.budgetRef.current.value;
         //Check input
-        if(inputIsCorrect){
+        if (inputIsCorrect) {
             this.props.editBudget(this.budgetRef.current.value);
             this.budgetRef.current.value = "";
-        }else{
+        } else {
             alert("Input format is incorrect!");
         }
     }
 
-    
 
-    editName = function(){
+
+    editName = function () {
         this.props.editName(this.nameRef.current.value);
         this.nameRef.current.value = "";
     }
 
-    budgetFieldKeyDown = function(e){
-        if(e.keyCode===13)this.editBudget();
+    budgetFieldKeyDown = function (e) {
+        if (e.keyCode === 13) this.editBudget();
     }
 
-    nameFieldKeyDown = function(e){
+    nameFieldKeyDown = function (e) {
         var key = e.keyCode;
-        if(key===13)this.editName();
+        if (key === 13) this.editName();
     }
 
     render() {
@@ -45,15 +45,23 @@ export class Settings extends Component {
                     <h1>Settings</h1>
                     <div className="tabField">
                         <h2>Monthly budget</h2>
-                        <input type="number" placeholder={this.props.state.monthly_budget} className="textField" ref={this.budgetRef} onKeyDown={this.budgetFieldKeyDown.bind(this)}></input>
-                        <input type="button" value="Update" className="button" onClick={this.editBudget}></input>
-                        <input type="button" value="Delete" className="button deleteButton" onClick={this.props.deleteBudget}></input>
+                        <div className="inline-flex center">
+                            <input type="number" placeholder={this.props.state.monthly_budget} className="textField" ref={this.budgetRef} onKeyDown={this.budgetFieldKeyDown.bind(this)}></input>
+                            <div className="inline-flex center">
+                                <input type="button" value="Update" className="button" onClick={this.editBudget}></input>
+                                <input type="button" value="Delete" className="button deleteButton" onClick={this.props.deleteBudget}></input>
+                            </div>
+                        </div>
                     </div>
                     <div className="tabField">
                         <h2>Name</h2>
-                        <input type="text" placeholder={this.props.state.name} className="textField" ref={this.nameRef} onKeyDown={this.nameFieldKeyDown.bind(this)}></input>
-                        <input type="button" value="Update" className="button" onClick={this.editName}></input>
-                        <input type="button" value="Delete" className="button deleteButton" onClick={this.props.deleteName}></input>
+                        <div className="inline-flex center">
+                            <input type="text" placeholder={this.props.state.name} className="textField" ref={this.nameRef} onKeyDown={this.nameFieldKeyDown.bind(this)}></input>
+                            <div className="inline-flex center">
+                                <input type="button" value="Update" className="button" onClick={this.editName}></input>
+                                <input type="button" value="Delete" className="button deleteButton" onClick={this.props.deleteName}></input>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
